8ada167269aea2d98aefde03523ce41e11867921
lakshrajkumar26 <110077095+lakshrajkumar26@users.noreply.github.com>
2025-09-10 09:21:34 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,1 +1,15 @@
 
+import { Navigate } from "react-router-dom";
+import  useAppStore  from "../store/useAuthStore";
+
+const ProtectedRoute = ({ children }) => {
+  const { token, user } = useAppStore();
+
+  if (!user || !token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
